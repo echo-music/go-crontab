f9a68c021bd03edcd6e0426f0ea634b62bd42f67
echo music <php_fangting@126.com>
2021-07-18 15:13:54 +0800

    save

--- master/api/task_save.go
+++ master/api/task_save.go
@@ -1,13 +1,38 @@
 package api
 
 import (
+	"context"
+	"fmt"
+	etcdv3 "github.com/coreos/etcd/clientv3"
 	"github.com/echo-music/go-crontab/master/common"
+	"github.com/echo-music/go-crontab/master/model/etcd"
 	"net/http"
 )
 
-func TaskSave(resp http.ResponseWriter, req *http.Request) {
+func TaskSave( resp http.ResponseWriter,req *http.Request)  {
 
-	reply, _ := common.BuildResponse(0, "success", "")
+	var (
+		client *etcdv3.Client
+		putRes *etcdv3.PutResponse
+		err error
+	)
 
-	resp.Write(reply)
+	fmt.Println(req)
+
+	client = etcd.EtcdClient()
+	kv := etcdv3.NewKV(client)
+
+	//添加任务
+	if putRes, err = kv.Put(context.TODO(), "/cron/task/task1", "hello1"); err != nil {
+		reply, _ := common.BuildResponse(0, err.Error(), "")
+		_, err = resp.Write(reply)
+
+
+	} else {
+		reply, _ := common.BuildResponse(0, "success", putRes)
+		_, err = resp.Write(reply)
+	}
+
+
+
 }
