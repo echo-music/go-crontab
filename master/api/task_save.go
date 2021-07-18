015ab529ced04bd023a1edb6cf728413c8e104db
echo music <php_fangting@126.com>
2021-07-18 15:29:27 +0800

    分布式任务调度

--- master/api/task_save.go
+++ master/api/task_save.go
@@ -2,37 +2,40 @@ package api
 
 import (
 	"context"
-	"fmt"
 	etcdv3 "github.com/coreos/etcd/clientv3"
 	"github.com/echo-music/go-crontab/master/common"
 	"github.com/echo-music/go-crontab/master/model/etcd"
 	"net/http"
 )
 
-func TaskSave( resp http.ResponseWriter,req *http.Request)  {
+//添加任务
+func TaskSave(resp http.ResponseWriter, req *http.Request) {
 
 	var (
 		client *etcdv3.Client
 		putRes *etcdv3.PutResponse
-		err error
+		err    error
 	)
 
-	fmt.Println(req)
+	if req.Method != "get" {
+		reply, _ := common.BuildResponse(401,"拒绝请求", "")
+		_, err = resp.Write(reply)
+		return
+	}
 
 	client = etcd.EtcdClient()
 	kv := etcdv3.NewKV(client)
 
 	//添加任务
 	if putRes, err = kv.Put(context.TODO(), "/cron/task/task1", "hello1"); err != nil {
-		reply, _ := common.BuildResponse(0, err.Error(), "")
+		reply, _ := common.BuildResponse(401, err.Error(), "")
 		_, err = resp.Write(reply)
-
+		return
 
 	} else {
 		reply, _ := common.BuildResponse(0, "success", putRes)
 		_, err = resp.Write(reply)
+		return
 	}
-
-
 
 }
