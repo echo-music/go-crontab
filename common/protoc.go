3a50a2d5dacd98d9382c26e801da802e32c365ef
echo music <php_fangting@126.com>
2021-07-20 22:48:40 +0800

    调度器

--- common/protoc.go
+++ common/protoc.go
@@ -1,26 +1,13 @@
 package common
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/echo-music/go-crontab/work/model/etcd"
 	"github.com/gorhill/cronexpr"
 	"strings"
 	"time"
 )
-
-// 定时任务
-type Task struct {
-	Name     string `json:"name"`     //  任务名
-	Command  string `json:"command"`  // shell命令
-	CronExpr string `json:"cronExpr"` // cron表达式
-}
-
-// 任务调度计划
-type TaskSchedulePlan struct {
-	Job      *Task                // 要调度的任务信息
-	Expr     *cronexpr.Expression // 解析好的cronexpr表达式
-	NextTime time.Time            // 下次调度时间
-}
 
 // HTTP接口应答
 type Response struct {
@@ -67,6 +54,38 @@ func BuildJobEvent(eventType int, task *Task) (jobEvent *TaskEvent) {
 		EventType: eventType,
 		Task:      task,
 	}
+}
+
+// 定时任务
+type Task struct {
+	Name     string `json:"name"`     //  任务名
+	Command  string `json:"command"`  // shell命令
+	CronExpr string `json:"cronExpr"` // cron表达式
+}
+
+// 任务调度计划
+type TaskSchedulePlan struct {
+	Job      *Task                // 要调度的任务信息
+	Expr     *cronexpr.Expression // 解析好的cronexpr表达式
+	NextTime time.Time            // 下次调度时间
+}
+
+// 任务执行状态
+type TaskExecuteInfo struct {
+	task       *Task              // 任务信息
+	PlanTime   time.Time          // 理论上的调度时间
+	RealTime   time.Time          // 实际的调度时间
+	CancelCtx  context.Context    // 任务command的context
+	CancelFunc context.CancelFunc //  用于取消command执行的cancel函数
+}
+
+// 任务执行结果
+type JobExecuteResult struct {
+	ExecuteInfo *TaskExecuteInfo // 执行状态
+	Output      []byte           // 脚本输出
+	Err         error            // 脚本错误原因
+	StartTime   time.Time        // 启动时间
+	EndTime     time.Time        // 结束时间
 }
 
 // 变化事件
--- work/logic/scheduler.go
+++ work/logic/scheduler.go
@@ -1,1 +1,32 @@
 package logic
+
+import (
+	"github.com/echo-music/go-crontab/common"
+	"sync"
+)
+
+// 任务调度
+type Scheduler struct {
+	taskEventChan chan *common.TaskEvent	//  etcd任务事件队列
+	taskPlanTable map[string]*common.TaskSchedulePlan // 任务调度计划表
+	taskExecutingTable map[string]*common.TaskExecuteInfo // 任务执行表
+	taskResultChan chan *common.JobExecuteResult	// 任务结果队列
+}
+
+var once  sync.Once
+var _scheduler *Scheduler
+//初始化调度器
+
+func init()  {
+	once.Do(func() {
+		_scheduler = &Scheduler{
+			taskEventChan:      make(chan *common.TaskEvent,1000),
+			taskPlanTable:      nil,
+			taskExecutingTable: nil,
+			taskResultChan:     nil,
+		}
+	})
+}
+
+
+//推送事件给调度器
